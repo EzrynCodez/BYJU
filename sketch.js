1517264ba1214cf60c19b7292a69d1a73403f5c6
EzrynCodez <73193940+EzrynCodez@users.noreply.github.com>
2021-10-05 17:42:38 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var square;
+
+
+
+function setup() {
+  createCanvas(500,500);
+  square = createSprite(200, 200, 50, 50);
+}
+
+function draw() 
+{
+  background(90);
+
+  drawSprites();
+}
+
+
+
